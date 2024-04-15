71e421d044757015de9d8ed88cb15becdaaa59d2
phora <phora@10.60.59.193>
2024-04-15 17:55:31 +0200

    doctorPOJOUNcompleted

---
+++ Medical History/src/POJOS/Doctor.java
@@ -0,0 +1,21 @@
+package POJOS;
+
+public class Doctor {
+
+	private int doctor_id;
+	private String name;
+	private String speciality;
+	private String contact;
+
+	
+	public static void main(String[] args) {
+       
+    }
+	
+	public Doctor(int doctor_id, String name, String speciality, String contact) {
+		this.doctor_id= doctor_id;
+		this.name= name;
+		this.speciality=speciality;
+		
+	}
+}

0e063b3d453b0ddb261432924442ba67d46771c0
MariaMM04 <maria.martinmartinez@usp.ceu.es>
2024-04-15 18:15:29 +0200

    Test.

--- Medical History/src/Data_Base/Main.java
+++ Medical History/src/Data_Base/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hi world!");
-		System.out.println("Bye world");
+		System.out.println("Bye worldo");
 	}
 
 }

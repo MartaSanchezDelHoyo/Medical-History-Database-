38b535a2c565276c975f79c9e40edf662ff665b0
luisfelipe.rieraaldanondo@usp.ceu.es <luisfelipe.rieraaldanondo@usp.ceu.es>
2024-04-15 17:41:24 +0200

    LuisPOJOScreation

--- Medical History/src/Data_Base/Main.java
+++ Medical History/src/Data_Base/Main.java
@@ -3,8 +3,8 @@ package Data_Base;
 public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hi world!");
-System.out.println("Bye world");
+		System.out.println("Hi world!");
+		System.out.println("Bye world");
 	}
 
 }

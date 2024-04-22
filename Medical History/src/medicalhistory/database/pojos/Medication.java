2c9c691c73ece9f986d7f802621f2d3fa16dabb4
luisfelipe.rieraaldanondo@usp.ceu.es <luisfelipe.rieraaldanondo@usp.ceu.es>
2024-04-22 17:46:39 +0200

    medication works now

--- Medical History/src/medicalhistory/database/pojos/Medication.java
+++ Medical History/src/medicalhistory/database/pojos/Medication.java
@@ -1,36 +1,5 @@
 package medicalhistory.database.pojos;
 
-public class Medication {
-
-	private int medication_id;
-	private String type;
-	
-
-	
-	
-	public Medication(int medication_id, String type) {
-		this.setMedication_id(medication_id);
-				this.type=type;
-	}
-
-	public int getMedication_id() {
-		return medication_id;
-	}
-
-	public void setMedication_id(int medication_id) {
-		this.medication_id = medication_id;
-	}
-	
-	public String getType() {
-		return type;
-	}
-	public void setType(String type) {
-		this.type=type;
-	}
-}
-=======
-package medicalhistory.database.pojos;
-
 import java.util.Objects;
 
 public class Medication {
@@ -81,4 +50,4 @@ public class Medication {
 		this.type=type;
 	}
 }
->>>>>>> branch 'master' of https://github.com/MartaSanchezDelHoyo/Medical-History-Database-.git
+

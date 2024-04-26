7206489b2a36ca9ec6be3450d6a235b6ca4a994e
MariaMM04 <maria.martinmartinez@usp.ceu.es>
2024-04-26 18:30:28 +0200

    metodos de pojo hospital

--- Medical History/src/medicalhistory/database/interfaces/HospitalManager.java
+++ Medical History/src/medicalhistory/database/interfaces/HospitalManager.java
@@ -1,5 +1,16 @@
 package medicalhistory.database.interfaces;
 
+import java.util.ArrayList;
+import medicalhistory.database.pojos.*;
+
 public interface HospitalManager {
+
+	public Visit showVisit(ArrayList<Visit> list_visits, Integer visit_id);
+	public Treatment showTreatment(ArrayList<Treatment> list_treatment, Integer treatmentID);
+	public Medication showMedication(ArrayList<Medication> list_medication, int medication_id);
+	public void addTest (ArrayList<Test> list_test);
+	public void addMedication (ArrayList<Medication> list_medication);
+	public void addManufacturer (ArrayList<Manufacturer> list_manufacturer);
+	public void modifyMedication (ArrayList<Medication> list_medication, Integer manufacturerID); 
 
 }

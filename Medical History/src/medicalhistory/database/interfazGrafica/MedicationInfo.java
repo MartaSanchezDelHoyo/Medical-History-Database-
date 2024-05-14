e74852e16234fcae9d02ed6d721d3999d27f10f0
CEULuisFelipeRiera <luisfelipe.rieraaldanondo@usp.ceu.es>
2024-05-14 17:14:06 +0200

    interface

--- Medical History/src/medicalhistory/database/interfazGrafica/MedicationInfo.java
+++ Medical History/src/medicalhistory/database/interfazGrafica/MedicationInfo.java
@@ -11,39 +11,44 @@ import java.awt.Font;
 public class MedicationInfo extends JFrame{
 	private JTextField textField;
 	public MedicationInfo() {
+		getContentPane().setForeground(new Color(255, 255, 255));
+		getContentPane().setFont(new Font("Dialog", Font.PLAIN, 24));
 		setBackground(new Color(255, 255, 255));
 		setTitle("MedicationInfo");
 		getContentPane().setLayout(null);
+        setSize(1600, 1000);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
 		
 		JLabel lblNewLabel = new JLabel("ID:");
-		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 18));
-		lblNewLabel.setBounds(138, 51, 73, 24);
+		lblNewLabel.setFont(new Font("Dialog", Font.BOLD, 24));
+		lblNewLabel.setBounds(538, 95, 73, 24);
 		getContentPane().add(lblNewLabel);
 		
 		JLabel lblNewLabel_2_1 = new JLabel("<dynamic>");
-		lblNewLabel_2_1.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		lblNewLabel_2_1.setBounds(174, 48, 136, 31);
+		lblNewLabel_2_1.setFont(new Font("Dialog", Font.PLAIN, 24));
+		lblNewLabel_2_1.setBounds(574, 92, 136, 31);
 		getContentPane().add(lblNewLabel_2_1);
 		
 		JLabel lblNewLabel_1 = new JLabel("Name:");
-		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 18));
-		lblNewLabel_1.setBounds(496, 51, 67, 24);
+		lblNewLabel_1.setFont(new Font("Dialog", Font.BOLD, 24));
+		lblNewLabel_1.setBounds(879, 95, 84, 24);
 		getContentPane().add(lblNewLabel_1);
 		
 		JLabel lblNewLabel_2 = new JLabel("<dynamic>");
-		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		lblNewLabel_2.setBounds(564, 48, 136, 31);
+		lblNewLabel_2.setFont(new Font("Dialog", Font.PLAIN, 24));
+		lblNewLabel_2.setBounds(964, 92, 136, 31);
 		getContentPane().add(lblNewLabel_2);
 		
 		textField = new JTextField();
 		textField.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		textField.setColumns(1);
-		textField.setBounds(34, 141, 757, 198);
+		textField.setBounds(104, 232, 1333, 198);
 		getContentPane().add(textField);
 		
 		JLabel lblNewLabel_3 = new JLabel("Manufacturer");
-		lblNewLabel_3.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		lblNewLabel_3.setBounds(42, 100, 136, 31);
+		lblNewLabel_3.setFont(new Font("Tahoma", Font.BOLD, 22));
+		lblNewLabel_3.setBounds(104, 191, 177, 31);
 		getContentPane().add(lblNewLabel_3);
 	}
 	

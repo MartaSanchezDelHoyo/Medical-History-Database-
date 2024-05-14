d02f6082929c19b738bb6cc5c2a5054e04067af7
CEULuisFelipeRiera <luisfelipe.rieraaldanondo@usp.ceu.es>
2024-05-14 15:16:45 +0200

    medication manufacturer swing

--- Medical History/src/medicalhistory/database/interfazGrafica/ManufacturerInfo.java
+++ Medical History/src/medicalhistory/database/interfazGrafica/ManufacturerInfo.java
@@ -1,5 +1,49 @@
 package medicalhistory.database.interfazGrafica;
 
-public class ManufacturerInfo {
+import javax.swing.*;
 
+
+import medicalhistory.database.pojos.Patient;
+import medicalhistory.database.pojos.Visit;
+
+import java.awt.*;
+import java.awt.event.*;
+public class ManufacturerInfo extends JFrame{
+	private JTextField MedicationsField;
+	public ManufacturerInfo() {
+		setAlwaysOnTop(true);
+		setTitle("Manufacturers");
+		getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("ID:");
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 18));
+		lblNewLabel.setBounds(130, 54, 73, 24);
+		getContentPane().add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Name:");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 18));
+		lblNewLabel_1.setBounds(488, 54, 67, 24);
+		getContentPane().add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("<dynamic>");
+		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblNewLabel_2.setBounds(556, 51, 136, 31);
+		getContentPane().add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_2_1 = new JLabel("<dynamic>");
+		lblNewLabel_2_1.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblNewLabel_2_1.setBounds(166, 51, 136, 31);
+		getContentPane().add(lblNewLabel_2_1);
+		
+		MedicationsField = new JTextField();
+		MedicationsField.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		MedicationsField.setBounds(39, 158, 757, 198);
+		getContentPane().add(MedicationsField);
+		MedicationsField.setColumns(1);
+		
+		JLabel lblNewLabel_3 = new JLabel("Medications");
+		lblNewLabel_3.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblNewLabel_3.setBounds(47, 117, 136, 31);
+		getContentPane().add(lblNewLabel_3);
+	}
 }
--- Medical History/src/medicalhistory/database/interfazGrafica/MedicationInfo.java
+++ Medical History/src/medicalhistory/database/interfazGrafica/MedicationInfo.java
@@ -1,5 +1,51 @@
 package medicalhistory.database.interfazGrafica;
 
-public class MedicationInfo {
+import javax.swing.*;
+import medicalhistory.database.pojos.Patient;
+import medicalhistory.database.pojos.Visit;
+import java.awt.BorderLayout;
+import java.awt.Window.Type;
+import java.awt.Color;
+import java.awt.Font;
+
+public class MedicationInfo extends JFrame{
+	private JTextField textField;
+	public MedicationInfo() {
+		setBackground(new Color(255, 255, 255));
+		setTitle("MedicationInfo");
+		getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("ID:");
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 18));
+		lblNewLabel.setBounds(138, 51, 73, 24);
+		getContentPane().add(lblNewLabel);
+		
+		JLabel lblNewLabel_2_1 = new JLabel("<dynamic>");
+		lblNewLabel_2_1.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblNewLabel_2_1.setBounds(174, 48, 136, 31);
+		getContentPane().add(lblNewLabel_2_1);
+		
+		JLabel lblNewLabel_1 = new JLabel("Name:");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 18));
+		lblNewLabel_1.setBounds(496, 51, 67, 24);
+		getContentPane().add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("<dynamic>");
+		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblNewLabel_2.setBounds(564, 48, 136, 31);
+		getContentPane().add(lblNewLabel_2);
+		
+		textField = new JTextField();
+		textField.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		textField.setColumns(1);
+		textField.setBounds(34, 141, 757, 198);
+		getContentPane().add(textField);
+		
+		JLabel lblNewLabel_3 = new JLabel("Manufacturer");
+		lblNewLabel_3.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblNewLabel_3.setBounds(42, 100, 136, 31);
+		getContentPane().add(lblNewLabel_3);
+	}
+	
 
 }

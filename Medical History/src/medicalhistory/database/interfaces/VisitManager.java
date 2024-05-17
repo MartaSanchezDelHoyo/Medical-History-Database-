b38cc90e390caab4fd7fe350f5adbc5fe278a7d2
PabloMorales04 <pablo.hervalejomorales@usp.ceu.es>
2024-05-17 13:01:43 +0200

    ordened

--- Medical History/src/medicalhistory/database/interfaces/VisitManager.java
+++ Medical History/src/medicalhistory/database/interfaces/VisitManager.java
@@ -15,10 +15,10 @@ public interface VisitManager {
 	public void linkMedicationToVisit(Visit visit, Medication medi);
 	public void linkTreatmentToVisit(Visit visit, Treatment treat);
     public void changeVisit (Visit temporal);
+    public Visit getVisit (int visitID);
     public List<Visit> getVisitByHospital (int hospital_id);
     public List<Visit> getVisitByTest (int test_id);
     public List<Visit> getVisitByTreatment(int treatment_id);
-    public Visit getVisit (int visitID);
     public List<Visit> getVisitByDoctor (int doctor_id);
     public List<Visit> getVisitByPatient (int patient_id);
 

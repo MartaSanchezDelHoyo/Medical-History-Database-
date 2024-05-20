3490c81775c46aebaf77a445c8da3b52dfa7c2e9
CEULuisFelipeRiera <luisfelipe.rieraaldanondo@usp.ceu.es>
2024-05-20 18:08:46 +0200

    file to filepath change

--- Medical History/src/medicalhistory/database/jdbc/JDBCXMLManager.java
+++ Medical History/src/medicalhistory/database/jdbc/JDBCXMLManager.java
@@ -2,6 +2,11 @@ package medicalhistory.database.jdbc;
 
 import java.io.File;
 
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+
 import medicalhistory.database.interfaces.XMLManager;
 import medicalhistory.database.pojos.Patient;
 
@@ -14,15 +19,26 @@ public class JDBCXMLManager implements XMLManager  {
 	}
 
 	@Override
-	public Patient xml2Patient(File f) {
+	public Patient xml2Patient(String filepath) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/**
+	 * Simple transformation method. You can use it in your project.
+	 * @param sourcePath - Absolute path to source xml file.
+	 * @param xsltPath - Absolute path to xslt file.
+	 * @param resultDir - Directory where you want to put resulting files.
+	 */
 	@Override
-	public void xml2html(File xml, File xslt, File html) {
-		// TODO Auto-generated method stub
-		
+	public void xml2html(String sourcePath, String xsltPath,String resultDir) {
+		TransformerFactory tFactory = TransformerFactory.newInstance();
+		try {
+			Transformer transformer = tFactory.newTransformer(new StreamSource(new File(xsltPath)));
+			transformer.transform(new StreamSource(new File(sourcePath)),new StreamResult(new File(resultDir)));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
